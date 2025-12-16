4bf61e54fcc0bb9318d2984f155b6b09203d8396
LiteDuck1986 <emils2007smirnovs@gmail.com>
2025-12-16 10:31:49 +0200

    Ielikts laiks pēc kā varēs atkārtot loteriju un datums.

--- src/LotoEngine.java
+++ src/LotoEngine.java
@@ -1,9 +1,28 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Random;
 import java.util.Stack;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import javax.swing.JOptionPane;
 
 public class LotoEngine {
+	 static Timer timer;
+	
+	 // Laiks
+	public LotoEngine(int seconds) {
+        timer = new Timer();
+        timer.schedule(new RemindTask(), seconds*1000);
+	}
+
+    class RemindTask extends TimerTask {
+        public void run() {
+            System.out.println("Laiks beidzies!");
+            timer.cancel(); //beidz laiku
+        }
+    }
+
 
 	public static void main(String[] args) {
 		String izvele;
@@ -24,7 +43,12 @@ public class LotoEngine {
 			
 		switch(izvele) {
 		case "Sākt laimestu":
+			 LocalDateTime Datums = LocalDateTime.now();
+			 DateTimeFormatter Formatets = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+			 String formatetsDatums = Datums.format(Formatets);
 			
+			if(timer == null) {
+			Trauks.clear();
 			for(int i=0; i<10; i++) {
 				Trauks.push(rand.nextInt(10)+1);
 			}
@@ -42,6 +66,14 @@ public class LotoEngine {
 			JOptionPane.showMessageDialog(null,
 					"Laimests beidzies, atgriežaties pēc 1 minūtes!", "Informācija",
 					JOptionPane.INFORMATION_MESSAGE);
+			new LotoEngine(60);
+			break;
+			} else if(timer != null) {
+				JOptionPane.showMessageDialog(null,
+						"Laimests ir slēgts, nav vel pagājusi 1 minūte! Pēdējais laimests bija "+formatetsDatums, "Brīdinājums",
+						JOptionPane.WARNING_MESSAGE);
+				break;
+			}
 			break;
 			
 		case "Apskatīt laimestu":
@@ -53,7 +85,7 @@ public class LotoEngine {
 				laimigais3 = LaimigieSk.pop();
 				
 				JOptionPane.showMessageDialog(null,
-						"Tavi trīs laimīgie skaitļi: "+laimigais+", "+laimigais2+" un "+laimigais3+"!",
+						"Tavi trīs laimīgie skaitļi: "+laimigais3+", "+laimigais2+" un "+laimigais+"!",
 						"Paziņojums", JOptionPane.INFORMATION_MESSAGE);
 			
 			}else
